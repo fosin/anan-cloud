d06d2079beef7c46ad2e1bf09810506934d27d20
fosin <28860823@qq.com>
2020-04-07 09:29:41 +0800

    重构anan-oauth2模块

--- anan-cloudadviced/anan-adminserver/src/main/java/com/github/fosin/anan/AdminServerApplication.java
+++ anan-cloudadviced/anan-adminserver/src/main/java/com/github/fosin/anan/AdminServerApplication.java
@@ -1,18 +1,10 @@
 package com.github.fosin.anan;
 
 import com.github.fosin.anan.core.banner.AnanBanner;
-import com.github.fosin.anan.oauth2.annotation.EnableAnanOauth2;
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import com.github.fosin.anan.oauth2.annotation.EnableAnanResourceServer;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * Description
@@ -21,26 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  */
 @SpringBootApplication
 @EnableAdminServer
-@EnableAnanOauth2
-@EnableWebSecurity
+@EnableAnanResourceServer
 public class AdminServerApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(AdminServerApplication.class)
                 .banner(new AnanBanner("AnAn Admin Server"))
                 .logStartupInfo(true)
                 .run(args);
-    }
-
-    @Bean
-    LoadBalancerInterceptor loadBalancerInterceptor(LoadBalancerClient loadBalance) {
-        return new LoadBalancerInterceptor(loadBalance);
-    }
-
-    @Bean
-    @Primary
-    @RefreshScope
-    @ConfigurationProperties("spring.boot.admin")
-    public AdminServerProperties adminServerProperties() {
-        return new AdminServerProperties();
     }
 }
