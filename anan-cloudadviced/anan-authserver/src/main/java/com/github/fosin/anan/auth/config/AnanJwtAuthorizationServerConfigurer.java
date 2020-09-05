a2082a92b82cf4e6b664c03d752bbdaddaaa1668
fosin <28860823@qq.com>
2020-09-05 14:38:01 +0800

    精简目录移除anan-platform模块

--- anan-cloudadviced/anan-authserver/src/main/java/com/github/fosin/anan/auth/config/AnanJwtAuthorizationServerConfigurer.java
+++ anan-cloudadviced/anan-authserver/src/main/java/com/github/fosin/anan/auth/config/AnanJwtAuthorizationServerConfigurer.java
@@ -49,9 +49,6 @@ public class AnanJwtAuthorizationServerConfigurer extends AuthorizationServerCon
     /**
      * 用来配置客户端详情服务（ClientDetailsService），
      * 客户端详情信息在这里进行初始化，你能够把客户端详情信息写死在这里或者是通过数据库来存储调取详情信息。
-     *
-     * @param clients
-     * @throws Exception
      */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -62,8 +59,6 @@ public class AnanJwtAuthorizationServerConfigurer extends AuthorizationServerCon
     /**
      * 用来配置授权（authorization）以及令牌（token）的访问端点和令牌服务(token services)。
      * 配置身份认证器，配置认证方式，TokenStore，TokenGranter，OAuth2RequestFactory
-     *
-     * @param endpoints
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
@@ -176,7 +171,7 @@ public class AnanJwtAuthorizationServerConfigurer extends AuthorizationServerCon
         return (accessToken, authentication) -> {
             if (accessToken instanceof DefaultOAuth2AccessToken) {
                 DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
-                Map<String, Object> additionalInformation = new LinkedHashMap<String, Object>();
+                Map<String, Object> additionalInformation = new LinkedHashMap<>();
 //                    additionalInformation.put("code",ResultCode.SUCCESS.getCode());
 //                    additionalInformation.put("msg",ResultCode.SUCCESS.getMsg());
                 token.setAdditionalInformation(additionalInformation);
@@ -191,8 +186,6 @@ public class AnanJwtAuthorizationServerConfigurer extends AuthorizationServerCon
      * 对应于配置AuthorizationServer安全认证的相关信息，创建ClientCredentialsTokenEndpointFilter核心过滤器
      * 用来配置令牌端点(Token Endpoint)的安全约束.
      * 配置oauth认证安全策略 从表单提交经过OAuth认证
-     *
-     * @param security
      */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) {
@@ -209,11 +202,12 @@ public class AnanJwtAuthorizationServerConfigurer extends AuthorizationServerCon
         ;
     }
 
-    /**
-     * redis存储方式
-     *
-     * @return redisTokenStore
-     */
+//    /**
+//     *
+//     * redis存储方式
+//     *
+//     * @return redisTokenStore
+//     */
 //    @Bean
 //    public RedisTokenStore redisTokenStore() {
 //        RedisTokenStore redisTokenStore = new RedisTokenStore(redisConnectionFactory);
@@ -229,22 +223,22 @@ public class AnanJwtAuthorizationServerConfigurer extends AuthorizationServerCon
 //        return authenticationProvider;
 //    }
 
-    /**
-     *
-     * 这是一个基于JDBC的实现版本，令牌会被保存进关系型数据库。
-     * 使用这个版本的实现时，你可以在不同的服务器之间共享令牌信息，
-     * 使用这个版本的时候请注意把"spring-jdbc"这个依赖加入到你的classpath当中。
-     */
+//    /**
+//     *
+//     * 这是一个基于JDBC的实现版本，令牌会被保存进关系型数据库。
+//     * 使用这个版本的实现时，你可以在不同的服务器之间共享令牌信息，
+//     * 使用这个版本的时候请注意把"spring-jdbc"这个依赖加入到你的classpath当中。
+//     */
 //    @Bean
 //    public JdbcTokenStore jdbcTokenStore(){
 //        return new JdbcTokenStore(dataSource);
 //    }
 
-    /**
-     * 这个版本的实现是被默认采用的，它可以完美的工作在单服务器上（即访问并发量压力不大的情况下，并且它在失败的时候不会进行备份），
-     * 大多数的项目都可以使用这个版本的实现来进行尝试，你可以在开发的时候使用它来进行管理，因为不会被保存到磁盘中，所以更易于调试。
-     * @return
-     */
+//    /**
+//     * 这个版本的实现是被默认采用的，它可以完美的工作在单服务器上（即访问并发量压力不大的情况下，并且它在失败的时候不会进行备份），
+//     * 大多数的项目都可以使用这个版本的实现来进行尝试，你可以在开发的时候使用它来进行管理，因为不会被保存到磁盘中，所以更易于调试。
+//     * @return
+//     */
 //    @Bean
 //    public InMemoryTokenStore inMemoryTokenStore() {
 //        return new InMemoryTokenStore();
